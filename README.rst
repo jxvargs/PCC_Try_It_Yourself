0e38182003149064f5ee98a71dd7d130849fa62e
julianv <jjulianvargas@gmail.com>
2020-01-04 20:11:12 -0800

    Updating README...

--- README.rst
+++ README.rst
@@ -1,5 +1,4 @@
 Python Crash Course Book Exercises.
 ------------------------------------
 
-There are one to many ways to approach a problem.
-Let's begin...
+There are one to many ways to approach to a solution of a  problem so the goal is that the following solutions can be useful for the readers. Let's begin...
--- rivers.py
+++ rivers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3.7
-'''
-    JulianV
-    12/22/2019.
-    Exercise 6-5
-    Rivers and their countries.
-'''
-rivers = {'hudson': 
+
+# JulianV
+# 12/22/2019.
+# Exercise 6-5
+# Rivers and their countries.
+
+rivers = {'hudson':
     'It is a 316 mile river between New YorK city and Jersey city',
     'amazon':
     'The largest river in South America. Brazil, Peru, and Colombia.',
